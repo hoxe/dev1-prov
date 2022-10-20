40ab4b2533edaa49512747bed36edcbba8f7c468
Ernandes da Costa Joaquim <ernandesjoaquim@sad.infovia-mt>
2022-10-20 19:23:08 -0400

    First Commit

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate
+} from "react-router-dom";
+
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+
+import { AuthProvider, AuthContext } from "./contexts/auth";
+
+const AppRoutes = () => {
+    const Private = ({ children }) => {
+        const { authenticated } = useContext(AuthContext);
+
+        if (!authenticated) {
+            return <Navigate to="./login" />;
+                }
+
+            return children;
+    };
+     
+    return(
+        <Router>
+            <AuthProvider>
+            <Routes>
+                <Route exact path="/login" element={<LoginPage/>}/>
+                <Route exact path="/" element={<Private> <HomePage /></Private>}/>
+            </Routes>
+            </AuthProvider>
+        </Router>
+    )
+}
+
+export default AppRoutes;
